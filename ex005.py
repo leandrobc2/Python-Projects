6b445cd6ece7f518c78aba0a914cd242ee3a7d95
Leandro Cardoso <110910705+leandrobc2@users.noreply.github.com>
2023-07-25 10:39:23 -0400

    Add files via upload

---
+++ ex005.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um numero: '))
+n2 = n1-1
+n3 = n1+1
+print('O antecessor de {} é: {}\n e o sucessor é: {}'.format(n1, n2, n3))
+
+
+
+
---
+++ ex006.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um numero: '))
+n2 = n1*2
+n3 = n1*3
+n4 = n1**(1/2)
+print('O dobro de {} é: {}\n o triplo é {}\n a raiz quadrada é {:=^20.5f}'.format(n1, n2, n3, n4))
+
+
+
